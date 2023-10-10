62a9b65a6b1a424aac373c9196132613614e6cee
Aaditya Murthy <aadi.murthyananth@gmail.com>
2023-10-10 04:17:59 -0500

    Basic

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,20 @@
+use std::str::FromStr;
+
+use chess::{Board, ChessMove, Error, MoveGen, Piece, Square};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
+pub fn get_next_move(fen: &str) -> String {
+    let res: Result<Board, Error> = Board::from_str(fen);
+    let board = match res {
+        Ok(b) => b,
+        Err(_) => Board::default(),
+    };
+    let movegen = MoveGen::new_legal(&board);
 
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+    for chess_move in movegen {
+        // This move does not capture anything
+        return chess_move.to_string();
+    }
+    return ChessMove::new(Square::E4, Square::E4, Some(Piece::Queen)).to_string();
 }
